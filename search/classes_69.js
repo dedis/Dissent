480aa404e59caff884908d66b1430babf05ac0de
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-02-28 19:48:06 -0500

    documentation update

--- search/classes_69.js
+++ search/classes_69.js
@@ -3,6 +3,7 @@ var searchData=
   ['id',['Id',['../df/d5b/classDissent_1_1Connections_1_1Id.html',1,'Dissent::Connections']]],
   ['integer',['Integer',['../d7/d7c/classDissent_1_1Crypto_1_1Integer.html',1,'Dissent::Crypto']]],
   ['integerdata',['IntegerData',['../d9/d6a/classDissent_1_1Crypto_1_1IntegerData.html',1,'Dissent::Crypto']]],
+  ['ioverlaysender',['IOverlaySender',['../db/d06/classDissent_1_1Connections_1_1IOverlaySender.html',1,'Dissent::Connections']]],
   ['isender',['ISender',['../d9/d03/classDissent_1_1Messaging_1_1ISender.html',1,'Dissent::Messaging']]],
   ['isink',['ISink',['../d8/ddd/classDissent_1_1Messaging_1_1ISink.html',1,'Dissent::Messaging']]],
   ['isinkobject',['ISinkObject',['../dc/de6/classDissent_1_1Messaging_1_1ISinkObject.html',1,'Dissent::Messaging']]]
