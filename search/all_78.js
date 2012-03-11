dd5c9d4f7a24db36300930dc2a754bf5b09b9a2e
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-03-11 18:06:19 -0400

    documentation update

--- search/all_78.js
+++ search/all_78.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['xor_5fmessages',['xor_messages',['../d7/d60/structDissent_1_1Anonymity_1_1Tolerant_1_1AlibiData_1_1slot__data.html#ab612fe4b6caed70bb529871c3fe59eda',1,'Dissent::Anonymity::Tolerant::AlibiData::slot_data']]],
-  ['xormessagehashes',['XorMessageHashes',['../da/d70/classDissent_1_1Anonymity_1_1BulkRound_1_1Descriptor.html#af74234de8910386a372eb1ef37b30df6',1,'Dissent::Anonymity::BulkRound::Descriptor']]],
-  ['xormessages',['XorMessages',['../da/d9a/classDissent_1_1Anonymity_1_1Tolerant_1_1TolerantTreeRound.html#a57fce296ec7f15376dfa2594ac83aafa',1,'Dissent::Anonymity::Tolerant::TolerantTreeRound']]]
+  ['xormessagehashes',['XorMessageHashes',['../da/d70/classDissent_1_1Anonymity_1_1BulkRound_1_1Descriptor.html#af74234de8910386a372eb1ef37b30df6',1,'Dissent::Anonymity::BulkRound::Descriptor']]]
 ];
