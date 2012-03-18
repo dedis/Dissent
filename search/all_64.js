b50f6329edf85c8b19862cc0d672fb7d2092b6fb
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-03-18 14:49:51 -0400

    documentation update

--- search/all_64.js
+++ search/all_64.js
@@ -6,7 +6,6 @@ var searchData=
   ['defaultnetwork',['DefaultNetwork',['../d5/d45/classDissent_1_1Connections_1_1DefaultNetwork.html',1,'Dissent::Connections']]],
   ['defaultnetwork',['DefaultNetwork',['../d5/d45/classDissent_1_1Connections_1_1DefaultNetwork.html#a41e3b094396c4b65a8c93f770deb5aa1',1,'Dissent::Connections::DefaultNetwork']]],
   ['delay',['Delay',['../d3/de8/classDissent_1_1Transports_1_1BufferEdge.html#aee10f3915fde81230a0b187cea1ad140',1,'Dissent::Transports::BufferEdge']]],
-  ['deletekeys',['DeleteKeys',['../df/d28/classDissent_1_1Anonymity_1_1ShuffleRound.html#a439706bf8a579a9c9fae94dc5b9e47d8',1,'Dissent::Anonymity::ShuffleRound']]],
   ['demomode',['DemoMode',['../d7/dac/classDissent_1_1Applications_1_1Settings.html#abde6101ed681cbf07e9cb915240925be',1,'Dissent::Applications::Settings']]],
   ['descriptor',['Descriptor',['../da/d70/classDissent_1_1Anonymity_1_1BulkRound_1_1Descriptor.html',1,'Dissent::Anonymity::BulkRound']]],
   ['destructorcheck',['DestructorCheck',['../d4/d95/classDissent_1_1Utils_1_1StartStop.html#aa498a0da34ae54aecbd21d81e16e0d5d',1,'Dissent::Utils::StartStop']]],
