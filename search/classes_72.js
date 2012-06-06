d3f7709fb4b5af88e143f5a74fa218cfd7227204
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-06-06 01:47:26 -0400

    documentation update

--- search/classes_72.js
+++ search/classes_72.js
@@ -18,8 +18,8 @@ var searchData=
   ['roundcollector',['RoundCollector',['../d3/d51/classDissent_1_1Tests_1_1RoundCollector.html',1,'Dissent::Tests']]],
   ['roundidservice',['RoundIdService',['../d2/d7d/classDissent_1_1Web_1_1Services_1_1RoundIdService.html',1,'Dissent::Web::Services']]],
   ['roundstatemachine',['RoundStateMachine',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
-  ['roundstatemachine_3c_20csbulkround_20_3e',['RoundStateMachine< CSBulkRound >',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
-  ['roundstatemachine_3c_20neffkeyshuffle_20_3e',['RoundStateMachine< NeffKeyShuffle >',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
-  ['roundstatemachine_3c_20shuffleround_20_3e',['RoundStateMachine< ShuffleRound >',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
+  ['roundstatemachine_3c_20csbulkround_20_3e',['RoundStateMachine&lt; CSBulkRound &gt;',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
+  ['roundstatemachine_3c_20neffkeyshuffle_20_3e',['RoundStateMachine&lt; NeffKeyShuffle &gt;',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
+  ['roundstatemachine_3c_20shuffleround_20_3e',['RoundStateMachine&lt; ShuffleRound &gt;',['../d0/d6c/classDissent_1_1Anonymity_1_1RoundStateMachine.html',1,'Dissent::Anonymity']]],
   ['rpchandler',['RpcHandler',['../dc/d59/classDissent_1_1Messaging_1_1RpcHandler.html',1,'Dissent::Messaging']]]
 ];
---
+++ search/pages_64.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dissent_20_2d_20dining_2dcryptographers_2c_20shuffled_2dsend_20network',['Dissent - Dining-Cryptographers, Shuffled-Send Network',['../index.html',1,'']]]
+];
