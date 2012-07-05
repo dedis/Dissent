f660dd7062d3a7a032a6045b452054edaef5197e
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-07-05 16:44:37 -0400

    documentation update

--- search/all_65.js
+++ search/all_65.js
@@ -13,13 +13,14 @@ var searchData=
   ['encrypt',['Encrypt',['../d7/d96/classDissent_1_1Crypto_1_1AsymmetricKey.html#a6ecae307d1f3b4143b5fccfdf1a1cee4',1,'Dissent::Crypto::AsymmetricKey::Encrypt()'],['../df/d60/classDissent_1_1Crypto_1_1CppDsaPublicKey.html#a905f9460e7b0cbf4c458ed041510825d',1,'Dissent::Crypto::CppDsaPublicKey::Encrypt()'],['../df/dcd/classDissent_1_1Crypto_1_1CppPublicKey.html#abe5eba649e26735b4b269cc45fd3388b',1,'Dissent::Crypto::CppPublicKey::Encrypt()'],['../de/db3/classDissent_1_1Crypto_1_1NullPublicKey.html#adb503da47191d17b4df3df71be26f56c',1,'Dissent::Crypto::NullPublicKey::Encrypt()'],['../d5/d21/classDissent_1_1Crypto_1_1OnionEncryptor.html#a309baf3604c321fe4bb74e2d95b0497f',1,'Dissent::Crypto::OnionEncryptor::Encrypt()']]],
   ['entry',['Entry',['../d0/d0f/classDissent_1_1PeerReview_1_1Entry.html#ad53df4bb0dde3628f76982d62f06745d',1,'Dissent::PeerReview::Entry']]],
   ['entry',['Entry',['../d0/d0f/classDissent_1_1PeerReview_1_1Entry.html',1,'Dissent::PeerReview']]],
-  ['entrylog',['EntryLog',['../d0/d59/classDissent_1_1PeerReview_1_1EntryLog.html',1,'Dissent::PeerReview']]],
   ['entrylog',['EntryLog',['../d0/d59/classDissent_1_1PeerReview_1_1EntryLog.html#ab197cc70e169a1e78d26cb91441c54ab',1,'Dissent::PeerReview::EntryLog']]],
+  ['entrylog',['EntryLog',['../d0/d59/classDissent_1_1PeerReview_1_1EntryLog.html',1,'Dissent::PeerReview']]],
   ['entrytunnel',['EntryTunnel',['../d2/d1b/classDissent_1_1Tunnel_1_1EntryTunnel.html',1,'Dissent::Tunnel']]],
   ['entrytunnel',['EntryTunnel',['../d7/dac/classDissent_1_1Applications_1_1Settings.html#acbcae178f1264270b513948bf4e7560a',1,'Dissent::Applications::Settings::EntryTunnel()'],['../d2/d1b/classDissent_1_1Tunnel_1_1EntryTunnel.html#a81b13445a86d6ee969e5df3ce9a23a8d',1,'Dissent::Tunnel::EntryTunnel::EntryTunnel()']]],
   ['entrytunnelurl',['EntryTunnelUrl',['../d7/dac/classDissent_1_1Applications_1_1Settings.html#afa3bc2e3076ba461c5264ae58a0d4453',1,'Dissent::Applications::Settings']]],
   ['evidencestate',['EvidenceState',['../d7/d22/classDissent_1_1Anonymity_1_1Tolerant_1_1TolerantBulkRound.html#a230559c0d4914a01a35dd2cbe3cffe73',1,'Dissent::Anonymity::Tolerant::TolerantBulkRound']]],
   ['exittunnel',['ExitTunnel',['../dd/d24/classDissent_1_1Tunnel_1_1ExitTunnel.html',1,'Dissent::Tunnel']]],
   ['exittunnel',['ExitTunnel',['../d7/dac/classDissent_1_1Applications_1_1Settings.html#aaa74c4f9611d2a2a54bb85d7e4b2f9d9',1,'Dissent::Applications::Settings::ExitTunnel()'],['../dd/d24/classDissent_1_1Tunnel_1_1ExitTunnel.html#a3f0d24c75935e15636cd325d64aac74b',1,'Dissent::Tunnel::ExitTunnel::ExitTunnel()']]],
+  ['exittunnelproxyurl',['ExitTunnelProxyUrl',['../d7/dac/classDissent_1_1Applications_1_1Settings.html#a4d8a16140e2260eab65dd08039b5f497',1,'Dissent::Applications::Settings']]],
   ['expectedalibilength',['ExpectedAlibiLength',['../d8/dca/classDissent_1_1Anonymity_1_1Tolerant_1_1AlibiData.html#a3729ba5e10fdc9623a0e2d608ce41dee',1,'Dissent::Anonymity::Tolerant::AlibiData']]]
 ];
