dcdd927fc583d0a5719ac63978012891cad21837
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-09-02 03:45:10 -0400

    documentation update

--- search/classes_6d.js
+++ search/classes_6d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['messagerandomizer',['MessageRandomizer',['../d9/de3/classDissent_1_1Anonymity_1_1MessageRandomizer.html',1,'Dissent::Anonymity']]],
   ['messagewebservice',['MessageWebService',['../de/d5b/classDissent_1_1Web_1_1Services_1_1MessageWebService.html',1,'Dissent::Web::Services']]],
   ['mockedgehandler',['MockEdgeHandler',['../d1/d42/classDissent_1_1Tests_1_1MockEdgeHandler.html',1,'Dissent::Tests']]],
   ['mocksender',['MockSender',['../d0/d3a/classDissent_1_1Tests_1_1MockSender.html',1,'Dissent::Tests']]],
