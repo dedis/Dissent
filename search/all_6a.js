b44a72d63af4f41fba4a9a57571f500a8032a919
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-12-23 10:29:16 -0500

    cleanup

--- search/all_6a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['jsonpackager',['JsonPackager',['../dd/d79/classDissent_1_1Web_1_1Packagers_1_1JsonPackager.html',1,'Dissent::Web::Packagers']]]
-];
--- search/classes_75.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['udprequestpacket',['UdpRequestPacket',['../d0/d21/classDissent_1_1Tunnel_1_1Packets_1_1UdpRequestPacket.html',1,'Dissent::Tunnel::Packets']]],
-  ['udpresponsepacket',['UdpResponsePacket',['../d0/dee/classDissent_1_1Tunnel_1_1Packets_1_1UdpResponsePacket.html',1,'Dissent::Tunnel::Packets']]],
-  ['udpstartpacket',['UdpStartPacket',['../dd/dbd/classDissent_1_1Tunnel_1_1Packets_1_1UdpStartPacket.html',1,'Dissent::Tunnel::Packets']]]
-];
