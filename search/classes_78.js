83d12577bb1b61a75ec9e8ee9e07920405bc12c7
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2013-02-03 18:46:11 -0500

    f

--- search/classes_78.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['xorclientciphertext',['XorClientCiphertext',['../d2/d7b/classDissent_1_1Crypto_1_1BlogDrop_1_1XorClientCiphertext.html',1,'Dissent::Crypto::BlogDrop']]],
-  ['xorserverciphertext',['XorServerCiphertext',['../d0/d43/classDissent_1_1Crypto_1_1BlogDrop_1_1XorServerCiphertext.html',1,'Dissent::Crypto::BlogDrop']]]
-];
